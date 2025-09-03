2be8351e0e7b8964f452924c3c3a8408efec5d05
samjoshua7 <samjoshua.paldwin@gmail.com>
2025-09-03 15:48:42 +0530

    Update UI

---
+++ script.js
@@ -0,0 +1,47 @@
+// Show current time
+function updateTime() {
+  const now = new Date();
+  document.getElementById("time").innerText =
+    "🕒 " + now.toLocaleTimeString();
+}
+setInterval(updateTime, 1000);
+
+// Font size controls
+let fontSize = 16;
+function changeFontSize(action) {
+  if (action === "increase") fontSize += 2;
+  else if (action === "decrease") fontSize -= 2;
+  else fontSize = 16;
+  document.body.style.fontSize = fontSize + "px";
+}
+
+// Slider
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+// Auto slideshow
+setInterval(() => plusSlides(1), 6000);
